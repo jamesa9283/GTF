f41c07821fb154f002e31ab06d042e6f0d943e79
James Arthur <jamesarthur7@icloud.com>
2020-06-30 17:01:17 +0100

    Added real numbers for m instead of naturals

--- src/basic.lean
+++ src/basic.lean
@@ -1,6 +1,6 @@
 import tactic
 import data.real.basic 
-import data.complex.exponential
+import data.complex.exponential analysis.special_functions.pow
 
 noncomputable theory
 open_locale classical
@@ -37,58 +37,24 @@ that we define using θ and sinₘ(θ) is the y co-ordinate
 Will come back to wonky square thing, looks slightly confusing for the time
 being.  -/
 
-def radius (x : ℝ) (m : ℕ) := 1 / ( |sin x| ^ m + |cos x| ^ m ) ^ (1/m)
+def radius (x m : ℝ) := 1 / ( |sin x| ^ m + |cos x| ^ m ) ^ (1/m)
 
 -- def heavyside (x : ℝ) : ℝ := if x<0 then 0 else 1
 -- ^ ignore that, Kevin was trying to teach me something
 
-def sinm (θ : ℝ) (m : ℕ) := sin θ * radius θ m
+def sinm (θ m : ℝ) := sin θ * radius θ m
 -- trigonometric sin is defined!!
 
-def cosm (θ : ℝ) (m : ℕ) := cos θ * radius θ m
+def cosm (θ m : ℝ) := cos θ * radius θ m
 -- So we can onto prove somethings!
 -- actually, let us go and define a few more things 
 
-def tanm (θ : ℝ) (m : ℕ) := sinm θ m / cosm θ m
+def tanm (θ m : ℝ) := sinm θ m / cosm θ m
 
-def secm (θ : ℝ) (m : ℕ) := 1 / cosm θ m
+def secm (θ m : ℝ) := 1 / cosm θ m
 
-def cscm (θ : ℝ) (m : ℕ) := 1 / secm θ m
+def cscm (θ m : ℝ) := 1 / secm θ m
 
--- let us show that these functions follow general trigonometric convention
-
-lemma sinp_zero (m : ℕ): sinm 0 m = 0 :=
-begin
-  unfold sinm,
-  rw sin_zero,
-  rw zero_mul,
-end
-
--- we know that p q > 0 because the negative values get funny and annoying.
-
-lemma cosp_zero (m : ℕ) (hm : m ≠ 0) : cosm 0 m = 1 :=
-begin
-  unfold cosm,
-  unfold radius,
-  rw [cos_zero, one_mul, sin_zero, abs_zero, abs_one],
-  rw zero_pow',
-  norm_num,
-  exact hm,
-end
-
-lemma tanp_zero (m : ℕ) : tanm 0 m = 0 :=
-begin 
-  unfold tanm,
-  rw [sinp_zero, zero_div],
-end
-
-lemma secp_zero (m : ℕ) (hm : m ≠ 0) : secm 0 m = 1 :=
-begin
-  unfold secm,
-  rw cosp_zero, 
-  norm_num,
-  exact hm,
-end
 
 
 /-!
